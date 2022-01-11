74773ed2f828c0e19f29c0b12f0ad62e5a9f432c
Ahyeonn <ahyeon971030@gmail.com>
2022-01-10 21:19:26 -0800

    Users can view single post

--- blueprints/friend.py
+++ blueprints/friend.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from bson.objectid import ObjectId
 from extensions import *
 
-friend = Blueprint('friend', __name__, url_prefix="/friends")
+friend = Blueprint('friendss', __name__, url_prefix="/friends")
 
 # posts = [
 #     {'number': 1, 'title': 'Title', 'date': 'today'}
@@ -12,11 +13,11 @@ def friends_index():
     return render_template('/friends/friends_index.html', posts=posts.find())
 
 @friend.route('/new')
-def friends_new():
-    return render_template('/friends/friends_new.html')
+def friend_new():
+    return render_template('/friends/friend_new.html')
 
 @friend.route('/add_friend', methods=['POST'])
-def friends_submit():
+def friend_submit():
     post = {
         'nickname': request.form.get('nickname'),
         'friendcode': request.form.get('friendcode'),
@@ -24,4 +25,9 @@ def friends_submit():
         'description': request.form.get('description')
     }
     posts.insert_one(post)
-    return redirect(url_for('friend.friends_index'))
+    return redirect(url_for('friend.friend_show', post_id=post['_id']))
+
+@friend.route('/<post_id>')
+def friend_show(post_id):
+    post = posts.find_one({'_id': ObjectId(post_id)})
+    return render_template('/friends/friend_show.html', post=post)
