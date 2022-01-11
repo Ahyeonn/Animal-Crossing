f890236abb939d1c990ddcfd49ae8f24cae31275
Ahyeonn <ahyeon971030@gmail.com>
2022-01-10 20:17:11 -0800

    Users can post their information

--- blueprints/friend.py
+++ blueprints/friend.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from extensions import *
 
 friend = Blueprint('friend', __name__, url_prefix="/friends")
@@ -8,5 +8,20 @@ friend = Blueprint('friend', __name__, url_prefix="/friends")
 # ]
 
 @friend.route('/')
-def friend_index():
+def friends_index():
     return render_template('/friends/friends_index.html', posts=posts.find())
+
+@friend.route('/new')
+def friends_new():
+    return render_template('/friends/friends_new.html')
+
+@friend.route('/add_friend', methods=['POST'])
+def friends_submit():
+    post = {
+        'nickname': request.form.get('nickname'),
+        'friendcode': request.form.get('friendcode'),
+        'title': request.form.get('title'),
+        'description': request.form.get('description')
+    }
+    posts.insert_one(post)
+    return redirect(url_for('friend.friends_index'))
