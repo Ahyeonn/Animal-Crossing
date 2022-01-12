4118ec1da1d9c3ca0adcee693f03bd02a228ccc9
Ahyeonn <ahyeon971030@gmail.com>
2022-01-11 23:56:20 -0800

    Users can delete comments

--- blueprints/comment.py
+++ blueprints/comment.py
@@ -4,7 +4,7 @@ from extensions import *
 
 comment = Blueprint('comment', __name__, url_prefix='/friends')
 
-@comment.route('/comment', methods=['POST'])
+@comment.route('/comments', methods=['POST'])
 def comment_new(): #This post_id is to store post_id from the hidden value
     comment = {
         'post_id': ObjectId(request.form.get('post_id')),
@@ -12,5 +12,9 @@ def comment_new(): #This post_id is to store post_id from the hidden value
     }
     comments.insert_one(comment)
     return redirect(url_for('friend.friend_show', post_id=request.form.get('post_id'))) 
-    #This used for redirect to show page where we passed in post_id as a parameter
+    #This used for redirect to show page where we passed in post_id as a parameter and got it from the hidden value as well
 
+@comment.route('/comments/<comment_id>', methods=['POST'])
+def comments_delete(comment_id):
+    comments.delete_one({'_id': ObjectId(comment_id)})
+    return redirect(url_for('friend.friend_show', post_id=request.form.get('post_id')))
