e9567cc0a2f3ecf08a9551d695ea79debf05e227
Ahyeon Jeon <ahyeonjeon@Ahyeons-MBP.attlocal.net>
2022-10-02 17:47:50 -0700

    fix: extensions.py

--- extensions.py
+++ extensions.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import os
 
 
-uri = os.environ.get('MONGODB_URI')
+uri = os.environ.get('MONGODB_URI', 'mongodb://localhost/AC')
 client = MongoClient(uri)
 db = client.get_default_database()
 
